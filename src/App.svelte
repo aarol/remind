7219e652f5f393f742b0bf328d33c8e04dcc4d24
aaroluo <aaro.luo@gmail.com>
2022-11-02 13:18:47 +0200

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  let reminders = ["google.com"];
+</script>
+
+<main>
+  <h1>Reminders</h1>
+
+  {#each reminders as reminder}
+    <p>{reminder}</p>
+  {/each}
+
+  <p class="read-the-docs">Click on the Vite and Svelte logos to learn more</p>
+</main>
